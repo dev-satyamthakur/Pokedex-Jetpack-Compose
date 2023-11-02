8721c831d4a76ac15b015d514b747b8e86768870
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-02 20:47:57 +0530

    Creating Retrofit funtions in AppModule

--- app/src/main/java/com/satyamthakur/learning/pokedexcompose/di/AppModule.kt
+++ app/src/main/java/com/satyamthakur/learning/pokedexcompose/di/AppModule.kt
@@ -1,10 +1,31 @@
 package com.satyamthakur.learning.pokedexcompose.di
 
+import com.satyamthakur.learning.pokedexcompose.data.remote.PokeAPI
+import com.satyamthakur.learning.pokedexcompose.repository.PokemonRepository
+import com.satyamthakur.learning.pokedexcompose.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    fun providePokemonRepository(
+        api: PokeAPI
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeAPI = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+        .create(PokeAPI::class.java)
+
 }
---
+++ app/src/main/java/com/satyamthakur/learning/pokedexcompose/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.satyamthakur.learning.pokedexcompose.util
+
+object Constants {
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
+}
