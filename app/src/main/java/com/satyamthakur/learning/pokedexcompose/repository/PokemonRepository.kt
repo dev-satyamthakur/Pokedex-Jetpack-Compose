377055e762ad8f0bdaf54d333bba2eba2177fb39
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-02 20:44:28 +0530

    Adding Repository Class

---
+++ app/src/main/java/com/satyamthakur/learning/pokedexcompose/repository/PokemonRepository.kt
@@ -0,0 +1,35 @@
+package com.satyamthakur.learning.pokedexcompose.repository
+
+import com.satyamthakur.learning.pokedexcompose.data.remote.PokeAPI
+import com.satyamthakur.learning.pokedexcompose.data.remote.responses.Pokemon
+import com.satyamthakur.learning.pokedexcompose.data.remote.responses.PokemonList
+import com.satyamthakur.learning.pokedexcompose.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class PokemonRepository @Inject constructor(
+    private val api: PokeAPI
+) {
+
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
+        val response = try {
+            api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return Resource.Error("An unknown error occured.")
+        }
+
+        return Resource.Success(response)
+    }
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        val response = try {
+            api.getPokemonInfo(pokemonName)
+        } catch (e: Exception) {
+            return Resource.Error("An unknown error occured.")
+        }
+
+        return Resource.Success(response)
+    }
+
+}
