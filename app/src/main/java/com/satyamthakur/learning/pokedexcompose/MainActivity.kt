13dd658c8ddeaa4645744c17337752df26304acb
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-03 20:26:13 +0530

    Adding PokemonListScreen composable

--- app/src/main/java/com/satyamthakur/learning/pokedexcompose/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/pokedexcompose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.satyamthakur.learning.pokedexcompose.pokemonlist.PokemonListScreen
 import com.satyamthakur.learning.pokedexcompose.ui.theme.PokedexComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     navController = navController, startDestination = "pokemon_list_screen"
                 ) {
                     composable("pokemon_list_screen") {
-
+                        PokemonListScreen(navController)
                     }
                     composable(
                         "pokemon_list_screen/{dominantColor}/{pokemonName}",
